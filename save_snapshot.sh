77cf9a287fd65d3f78af23592ee2c70d3fd09525
sypper-pit <uuhn.info@gmail.com>
2024-12-25 21:51:16 +0300

    Update save_snapshot.sh

--- save_snapshot.sh
+++ save_snapshot.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-# Request container name
+# Получаем текущую директорию, где находится скрипт
+export_path="$(pwd)"
+
+# Запрос имени контейнера
 read -p "Enter container name: " container_name
 
-# Get list of snapshots
+# Получение списка снапшотов
 echo "Available snapshots for container $container_name:"
 lxc info $container_name | sed -n '/Snapshots:/,/^$/p' | tail -n +2
 
-# Request snapshot name
+# Запрос имени снапшота
 read -p "Enter snapshot name to save: " snapshot_name
 
-# Generate filename for saving
+# Формирование имени файла для сохранения
 filename="${container_name}_${snapshot_name}_$(date +"%Y%m%d_%H%M%S")_$(lxc version | awk '/Server version:/ {print $3}')_$(hostname -I | awk '{print $1}')"
 
-# Save snapshot
+# Сохранение снапшота как образа
 lxc publish $container_name/$snapshot_name --alias $filename
 
-echo "Snapshot saved as image with name: $filename"
+# Экспорт образа в текущую директорию
+lxc image export $filename $export_path/$filename
+
+echo "Snapshot exported to: $export_path/$filename.tar.gz"
