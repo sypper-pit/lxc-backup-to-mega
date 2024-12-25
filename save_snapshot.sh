461521b29e5b612656fb9ba887a6297b95528507
sypper-pit <uuhn.info@gmail.com>
2024-12-25 22:16:16 +0300

    Update save_snapshot.sh

--- save_snapshot.sh
+++ save_snapshot.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e  # Прекращает выполнение скрипта при любой ошибке
+
 # Получаем текущую директорию, где находится скрипт
 export_path="$(pwd)"
 
@@ -13,14 +15,17 @@ lxc info $container_name | sed -n '/Snapshots:/,/^$/p' | tail -n +2
 # Запрос имени снапшота
 read -p "Enter snapshot name to save: " snapshot_name
 
-# Формирование имени файла для сохранения
+# Формирование уникального имени файла для сохранения
 filename="${container_name}_${snapshot_name}_$(date +"%Y%m%d_%H%M%S")_$(lxc version | awk '/Server version:/ {print $3}')_$(hostname -I | awk '{print $1}' | tr '.' '-')"
+unique_alias="temp_export_$(date +%s)"
 
-# Сохранение снапшота как образа
-lxc publish $container_name/$snapshot_name --alias $filename
+# Сохранение снапшота как временного образа
+echo "Creating temporary image..."
+lxc publish $container_name/$snapshot_name --alias $unique_alias
 
 # Экспорт образа в текущую директорию
-lxc image export $filename "$export_path/${filename}.tar.gz"
+echo "Exporting image..."
+lxc image export $unique_alias "$export_path/${filename}.tar.gz"
 
 # Проверка, создался ли файл
 if [ -f "$export_path/${filename}.tar.gz" ]; then
@@ -30,4 +35,7 @@ else
 fi
 
 # Удаление временного образа
-lxc image delete $filename
+echo "Cleaning up..."
+lxc image delete $unique_alias
+
+echo "Operation completed."
